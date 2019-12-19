821a350bba78b0f94de8506084448567571ab9ee
olivier <olivier.costa@aegisoft.be>
2019-12-19 14:22:11 +0100

    make golden code-file warning free

--- python/test_golden_master.py
+++ python/test_golden_master.py
@@ -4,6 +4,7 @@ from gilded_rose import Item, GildedRose
 
 class GoldenMasterTest(unittest.TestCase):
     def test_golden_master(self):
+        output_file = None
         try:
             output_file = open("output.txt", 'r')
             golden_master_lines = [output_file.readlines()]
@@ -13,23 +14,23 @@ class GoldenMasterTest(unittest.TestCase):
 
         lines = golden_master_test_run()
 
-        for i in range(len(golden_master_lines) -1):
+        for i in range(len(golden_master_lines) - 1):
             self.assertEquals(golden_master_lines[i], lines[i])
 
 
 def golden_master_test_run():
     lines = ["OMGHAI!"]
     items = [
-             Item(name="+5 Dexterity Vest", sell_in=10, quality=20),
-             Item(name="Aged Brie", sell_in=2, quality=0),
-             Item(name="Elixir of the Mongoose", sell_in=5, quality=7),
-             Item(name="Sulfuras, Hand of Ragnaros", sell_in=0, quality=80),
-             Item(name="Sulfuras, Hand of Ragnaros", sell_in=-1, quality=80),
-             Item(name="Backstage passes to a TAFKAL80ETC concert", sell_in=15, quality=20),
-             Item(name="Backstage passes to a TAFKAL80ETC concert", sell_in=10, quality=49),
-             Item(name="Backstage passes to a TAFKAL80ETC concert", sell_in=5, quality=49),
-             Item(name="Conjured Mana Cake", sell_in=3, quality=6),  # <-- :O
-            ]
+        Item(name="+5 Dexterity Vest", sell_in=10, quality=20),
+        Item(name="Aged Brie", sell_in=2, quality=0),
+        Item(name="Elixir of the Mongoose", sell_in=5, quality=7),
+        Item(name="Sulfuras, Hand of Ragnaros", sell_in=0, quality=80),
+        Item(name="Sulfuras, Hand of Ragnaros", sell_in=-1, quality=80),
+        Item(name="Backstage passes to a TAFKAL80ETC concert", sell_in=15, quality=20),
+        Item(name="Backstage passes to a TAFKAL80ETC concert", sell_in=10, quality=49),
+        Item(name="Backstage passes to a TAFKAL80ETC concert", sell_in=5, quality=49),
+        Item(name="Conjured Mana Cake", sell_in=3, quality=6),  # <-- :O
+    ]
 
     days = 2
     import sys
