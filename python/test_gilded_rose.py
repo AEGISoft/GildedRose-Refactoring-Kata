a47e79e46c26067896463644944ab13a26ac4adf
olivier <olivier.costa@aegisoft.be>
2019-12-19 14:23:19 +0100

    verify legendary item isn't updated

--- python/test_gilded_rose.py
+++ python/test_gilded_rose.py
@@ -5,11 +5,12 @@ from gilded_rose import Item, GildedRose
 
 
 class GildedRoseTest(unittest.TestCase):
-    def test_foo(self):
-        items = [Item("foo", 0, 0)]
+    def test_legendary_items_don_t_update(self):
+        items = [Item("Sulfuras, Hand of Ragnaros", -1, 80)]
         gilded_rose = GildedRose(items)
         gilded_rose.update_quality()
-        self.assertEquals("foo", items[0].name)
+        self.assertEquals(80, items[0].quality)
+        self.assertEquals(-1, items[0].sell_in)
 
 
 if __name__ == '__main__':
